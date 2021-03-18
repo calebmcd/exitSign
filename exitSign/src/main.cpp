6b264d39e746af521869836158d84b3e5fad885c
Caleb McDougall <caleb.mcdougall2@gmail.com>
2021-03-18 18:00:32 -0400

    Connected to RGB Lights with On/OFF Commands

--- exitSign/src/main.cpp
+++ exitSign/src/main.cpp
@@ -1,5 +1,4 @@
 // ToDo: @calebmcd Change this whole main file to account for RGB lights
-
 #include <WiFi.h>
  
 #include <Adafruit_MQTT.h>
@@ -8,10 +7,20 @@
 
 #include <fastled.h>
 
-#include <secret.h>
- 
-int output=2;
- 
+#include <secrets.h>
+
+// Fast LED Definitions
+#define LED_PIN     2
+#define NUM_LEDS    10
+#define BRIGHTNESS  64
+#define LED_TYPE    WS2812B
+#define COLOR_ORDER GRB
+CRGB leds[NUM_LEDS];
+
+CRGBPalette16 currentPalette;
+TBlendType    currentBlending;
+
+// AIO setup
 WiFiClient client;     // Create an ESP8266 WiFiClient class to connect to the MQTT server.
  
 Adafruit_MQTT_Client mqtt(&client, AIO_SERVER, AIO_SERVERPORT, AIO_USERNAME, AIO_KEY);        // Setup the MQTT client class by passing in the WiFi client and MQTT server and login details.
@@ -19,32 +28,100 @@ Adafruit_MQTT_Client mqtt(&client, AIO_SERVER, AIO_SERVERPORT, AIO_USERNAME, AIO
 Adafruit_MQTT_Subscribe LED_Control = Adafruit_MQTT_Subscribe(&mqtt, AIO_USERNAME "/feeds/exitSign_control");
  
 void MQTT_connect();
+
+bool greenFlag = false;
+
+// Flash a chosen color a certain number of times
+void flashGreen(){
+    
+  for(int i=0;i<3;i++){
+
+    fill_solid( leds, NUM_LEDS, CRGB::Green);
+    FastLED.show();
+    delay(500);
+
+    fill_solid( leds, NUM_LEDS, CRGB::Black);
+    FastLED.show();
+    delay(500);
+  }
+}
+
+void flashRed(){
+    
+  for(int i=0;i<3;i++){
+
+    fill_solid( leds, NUM_LEDS, CRGB::Red);
+    FastLED.show();
+    delay(500);
+
+    fill_solid( leds, NUM_LEDS, CRGB::Black);
+    FastLED.show();
+    delay(500);
+  }
+}
+
+struct CRGB toCRGB(uint8_t* inAIO){
+  char* char_value = (char*)inAIO;
+
+  //toupper(char_value[0]);
+
+  //return CRGB::char_value;
+
+}
+
+void SetupBlackAndRedStripedPalette()
+{
+    // 'black out' all 16 palette entries...
+    fill_solid( currentPalette, 16, CRGB::Black);
+    // and set every fourth one to white.
+    currentPalette[0] = CRGB::Red;
+    currentPalette[4] = CRGB::Red;
+    currentPalette[8] = CRGB::Red;
+    currentPalette[12] = CRGB::Red;
+    
+}
+
+// Function to fill LED array
+void FillLEDsFromPaletteColors( uint8_t colorIndex)
+{
+    uint8_t brightness = 255;
+    
+    for( int i = 0; i < NUM_LEDS; i++) {
+        leds[i] = ColorFromPalette( currentPalette, colorIndex, brightness, currentBlending);
+        colorIndex += 3;
+    }
+}
  
 void setup() {
+
+  delay(3000); // Power up delay
+
+  // Setup LED Strip
+  FastLED.addLeds<LED_TYPE, LED_PIN, COLOR_ORDER>(leds, NUM_LEDS).setCorrection( TypicalLEDStrip );
+  FastLED.setBrightness(  BRIGHTNESS );
+  
+  // Set LED's to linear blend Rainbow
+  currentPalette = RainbowColors_p;
+  currentBlending = LINEARBLEND;
  
   Serial.begin(9600);
  
-  delay(10);
- 
-pinMode(2,OUTPUT);
- 
- // Connect to WiFi access point.
+  // Connect to WiFi access point and set lights to Red
  
   Serial.println(); Serial.println();
  
   Serial.print("Connecting to ");
  
   Serial.println(WLAN_SSID);
- 
- 
- 
+  
+  SetupBlackAndRedStripedPalette();
+  
   WiFi.begin(WLAN_SSID, WLAN_PASS);
  
   while (WiFi.status() != WL_CONNECTED) {
  
-    delay(500);
- 
     Serial.print(".");
+    flashRed();
  
   }
  
@@ -78,25 +155,37 @@ Adafruit_MQTT_Subscribe *subscription;
  
       {
  
-        digitalWrite(2, HIGH);
+        //Serial.println("Turned on");
+        fill_solid(leds, NUM_LEDS, CRGB::Blue);
+        //fill_rainbow(leds, NUM_LEDS);
+        FastLED.show();
  
       }
  
-      else
+      else if (!strcmp((char*) LED_Control.lastread, "OFF"))
  
       {
  
-        digitalWrite(2, LOW);
- 
+        //Serial.println("Turned off");
+        fill_solid( leds, NUM_LEDS, CRGB::Black);
+        FastLED.show();
+    
+      }
+
+      else {
+
+        //const struct CRGB &color = toCRGB(LED_Control.lastread);
+
+        toCRGB(LED_Control.lastread);
+
+        //fill_solid( leds, NUM_LEDS, color);
+        //FastLED.show();
+
       }
  
     }
  
   }
- 
- 
- 
- 
  
 }
  
@@ -139,5 +228,9 @@ uint8_t retries = 3;
   }
  
   Serial.println("MQTT Connected!");
+  if(greenFlag==false) {
+    flashGreen();
+    greenFlag=true;
+  }
  
 }
